1d21408ce274190405f3ea1d9472f8e7518f3651
Andrew Eiche <aeiche@gmail.com>
2016-01-30 16:11:27 -0500

    rounding errors.

--- WBS99/Assets/Scripts/GameManagement/Round.cs
+++ WBS99/Assets/Scripts/GameManagement/Round.cs
@@ -46,6 +46,12 @@ public class Round {
 
 	//=ROUND(NORMINV(0.99*E11,B2/2,B2/6),0)
 	private int CalculateHits(){
+		if (hitProbability == 0) {
+			hitProbability = Mathf.Epsilon;
+		} else if (hitProbability == 1) {
+			hitProbability = 1 - Mathf.Epsilon;
+		}
+
 		if (atBats == 0) {
 			return 0;
 		}

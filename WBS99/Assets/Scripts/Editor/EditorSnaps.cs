c2c774f0b0afe180ded267c37783b1024f3f8535
Levi Lansing <helives@gmail.com>
2016-01-30 00:21:02 -0500

    Road Tiles and editor scipts

---
+++ WBS99/Assets/Scripts/Editor/EditorSnaps.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorSnaps : EditorWindow
+{
+	private Vector3 prevPosition;
+	private bool doSnap = true;
+	private float snapValue = 3;
+	private bool enforceYValue = true;
+	private float yValue = 0;
+
+	[MenuItem( "Edit/Auto Snap %_l" )]
+
+	static void Init()
+	{
+		var window = (EditorSnaps)EditorWindow.GetWindow( typeof( EditorSnaps ) );
+		window.maxSize = new Vector2( 200, 100 );
+	}
+
+	public void OnGUI()
+	{
+		doSnap = EditorGUILayout.Toggle( "Auto Snap", doSnap );
+//		snapValue = EditorGUILayout.FloatField( "Snap Value", snapValue );
+		enforceYValue = EditorGUILayout.Toggle( "Enforce Y", enforceYValue );
+		yValue = EditorGUILayout.FloatField( "Y Value", yValue );
+		if (GUILayout.Button ("Rotate")) {
+			Rotate ();
+		}
+	}
+
+	public void Update()
+	{
+		if ( doSnap
+			&& !EditorApplication.isPlaying
+			&& Selection.transforms.Length > 0
+			&& Selection.transforms[0].position != prevPosition )
+		{
+			Snap();
+			prevPosition = Selection.transforms[0].position;
+		}
+	}
+
+	private void Snap()
+	{
+		foreach ( var transform in Selection.transforms )
+		{
+			var t = transform.transform.position;
+			t.x = Round( t.x );
+			if (enforceYValue) {
+				t.y = yValue;
+			} else {
+				t.y = Round( t.y );
+			}
+			t.z = Round( t.z );
+			transform.transform.position = t;
+		}
+	}
+
+	private float Round( float input )
+	{
+		return snapValue * Mathf.Round( ( input / snapValue ) );
+	}
+
+	private void Rotate() {
+		foreach (var transform in Selection.transforms) {
+			transform.transform.RotateAround (transform.transform.position, Vector3.up, 90);
+		}
+	}
+}
---
+++ WBS99/Assets/Scripts/Editor/Prefabinator.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+public class Prefabinator {
+
+	[MenuItem("Tools/Prefabinator")]
+	private static void Prefabinate() {
+		string[] folders = { 
+			"Assets/Tiles/3D Road Tiles", 
+			"Assets/Tiles/Nature"
+		};
+		foreach (var folder in folders) {
+			string group = Path.GetFileName (folder);
+			RunWithFolder (folder, group);
+		}
+	}
+
+	private static void RunWithFolder(string folder, string group) {
+		var info = new DirectoryInfo(folder);
+
+		foreach (FileInfo file in info.GetFiles("*.obj")) {
+			string path = AssetPath (file.FullName);
+			Object o = AssetDatabase.LoadAssetAtPath<GameObject> (path);
+			var go = (GameObject)GameObject.Instantiate (o);
+			AddMeshCollider (go, path);
+			go.isStatic = true;
+			UnityEditor.PrefabUtility.CreatePrefab (PrefabPath(group, file.FullName), go);
+			GameObject.DestroyImmediate (go);
+		}
+	}
+
+	private static void AddMeshCollider(GameObject go, string filename) {
+		MeshFilter mf = go.GetComponentInChildren<MeshFilter> ();
+		if (mf != null) {
+			var mc = go.AddComponent<MeshCollider> ();
+			mc.sharedMesh = mf.sharedMesh;
+			mc.convex = true;
+
+		} else {
+			Debug.LogError ("Mesh Filter not found for " + Path.GetFileNameWithoutExtension (filename));
+		}
+	}
+
+	private static string AssetPath(string path) {
+		return "Assets" + path.Substring (Application.dataPath.Length);
+	}
+
+	private static string PrefabPath(string group, string path) {
+		string dir = Application.dataPath + "/Tiles/Prefabs/" + group;
+		if (!Directory.Exists(dir)) {
+			Directory.CreateDirectory (dir);
+		}
+		return "Assets/Tiles/Prefabs/" + group + "/" + Path.GetFileNameWithoutExtension (path) + ".prefab";
+	}
+}

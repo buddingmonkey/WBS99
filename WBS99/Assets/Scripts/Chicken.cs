b745b4868da5f208cea3e1b939eb8ad7a34237d9
Andrew Mshar <mshar_andrew@bah.com>
2016-01-30 01:09:46 -0500

    Created basic chicken and enemy scripts.

---
+++ WBS99/Assets/Scripts/Chicken.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chicken : MonoBehaviour {
+
+	[SerializeField]
+	private float range = 100;
+	private NavMeshAgent agent;
+	private Vector3 point;
+
+	bool RandomPoint(Vector3 center, float range, out Vector3 result) {
+		for (int i = 0; i < 30; i++) {
+			Vector3 randomPoint = center + Random.insideUnitSphere * range;
+			NavMeshHit hit;
+			if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) {
+				result = hit.position;
+				return true;
+			}
+		}
+		result = Vector3.zero;
+		return false;
+	}
+
+	protected bool pathComplete()
+	{
+		if (!agent.pathPending)
+		{
+			if (agent.remainingDistance <= agent.stoppingDistance)
+			{
+				if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+				{
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	void FindNewPoint ()
+	{
+		while (!RandomPoint (transform.position, range, out point)) {
+		}
+		//TODO: get rid of this debug
+		agent.destination = point;
+	}
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		FindNewPoint ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay (point, Vector3.up, Color.blue, 1.0f);
+		if(pathComplete()){
+			FindNewPoint();
+		}
+	}
+}
+
+
---
+++ WBS99/Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    private GameObject player;
+    private NavMeshAgent agent;
+    public float speed = 1.0f;
+ 
+    // Use this for initialization
+    void Start () {
+   
+   //TODO: Remove FIND!
+        player = GameObject.FindGameObjectWithTag ("Player");
+ 
+        if (!player) {
+            Debug.Log ("ERROR could not find Player!");
+        }
+        
+          agent = GetComponent<NavMeshAgent>();
+    }
+   
+    // Update is called once per frame
+    void Update () {
+ 
+        if (!player) {
+            return;
+        }
+ 
+          agent.destination = player.transform.position; 
+        // var distance = Vector3.Distance( player.transform.position, transform.position);
+ 
+        // if ( distance < 100  )  
+        // {  
+        //     //Debug.Log ("player is close");
+        //     var delta = player.transform.position - transform.position;
+        //     delta.Normalize();
+        //     var moveSpeed = speed * Time.deltaTime;
+        //     transform.position = transform.position + (delta * moveSpeed);
+        //     transform.LookAt(player.transform);
+        // }
+ 
+        // else    
+        // {
+        //     Debug.Log("not close yet " + distance);
+        // }
+    }
+}

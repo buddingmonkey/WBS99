3aa41c1faffb6304fdefbf180ecfdfa6ace1aeb3
Andrew Eiche <aeiche@gmail.com>
2016-01-29 22:32:37 -0500

    Creating game control and spawners for generic enemy / player

--- WBS99/Assets/Scripts/GameManagement/GameController.cs
+++ WBS99/Assets/Scripts/GameManagement/GameController.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public enum GameState{
+	Flying,
+	Baseball,
+	City,
+	Pausied,
+	GameOver
+}
 
 public class GameController : MonoBehaviour {
 
+	[SerializeField]
+	private Spawner playerSpawner;
+
+	private Transform player;
+
+	[SerializeField]
+	private List<Spawner> enemySpawners;
+
+	public GameState gameState { get; private set; }
+
 	// Use this for initialization
 	void Start () {
-	
+		gameState = GameState.Flying;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (gameState == GameState.Flying) {
+			gameState = GameState.City;
+			SpawnPlayer ();
+		}
+	}
+
+	private void SpawnPlayer(){
+		player = playerSpawner.Spawn ();
+	}
+
+	public void PlayBall(){
+		// Do some baseball shit in here
+		playerSpawner.DestroyObject(player);
+		Debug.Log("BASEBALL!");
+		gameState = GameState.Flying;
 	}
 }
---
+++ WBS99/Assets/Scripts/WorldObjects/BallPark.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallPark : MonoBehaviour {
+
+	[SerializeField]
+	private GameController gameController;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			gameController.PlayBall ();
+		}
+	}
+}

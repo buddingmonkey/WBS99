38b5b2d1fa551b612890c4a6f8f669de7a7fcca0
Andrew Mshar <mshar_andrew@bah.com>
2016-01-31 14:02:55 -0500

    Bat swingin'

---
+++ WBS99/Assets/Scripts/BatAnimationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatAnimationController : MonoBehaviour {
+
+	public GameObject Bat;
+
+	void EnableBat () {
+
+		Bat.SetActive (true);
+	}
+	void DisableBat () {
+
+		Bat.SetActive (false);
+	}
+
+}

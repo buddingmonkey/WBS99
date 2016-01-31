8fab7c6a96ab7f40601308ffb431d6b263312eb0
Andrew Eiche <aeiche@gmail.com>
2016-01-31 11:36:57 -0500

    awwww that sweet beef music

---
+++ WBS99/Assets/Scripts/BeefMusic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeefMusic : MonoBehaviour {
+	[SerializeField]
+	private AudioClip moo;
+	[SerializeField]
+	private AudioClip step;
+	// Use this for initialization
+
+	[SerializeField]
+	private AudioSource audioSource;
+
+	void Update(){
+		if (!audioSource.isPlaying ) {
+			if (Random.Range (0, 4) == 0) {
+				audioSource.clip = moo;
+			} else {
+				audioSource.clip = step;
+			}
+			audioSource.Play ();
+		}
+	}
+}

31ff0d53b2065ad5c4e032e1163895abf5a237be
Andrew Eiche <aeiche@gmail.com>
2016-01-31 13:32:10 -0500

    Winning

--- WBS99/Assets/Scripts/GameManagement/GameConstants.cs
+++ WBS99/Assets/Scripts/GameManagement/GameConstants.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class GameConstants
 {
 	public const float losingAverage = 0.100f;
-	public const int winningHits = 3010;
+	public const int winningHits = 301;
 	public const int gamesPerCity = 3;
 }
 
